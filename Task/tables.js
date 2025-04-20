dbe20775418886a5599fe2074844d02a7c997c83
Your Name <gchkumar999@gmail.com>
2025-04-21 00:04:38 +0530

    txt

---
+++ Task/tables.js
@@ -0,0 +1,16 @@
+// // for(let i=1 ;i<=10; i++){
+// //     console.log(2  + "x"  + i + "="+(2*i))
+// // }
+
+
+// let i=1
+// while(i<=10){
+//     console.log(5 + "x" + i + "=" +(5*i))
+//     i++
+// }
+
+let i=1
+do{
+    console.log(9 + "x" + i + "=" + (9*i))
+    i++
+}while(i<=10)
